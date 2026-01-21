ddbd684eaacaa497c34c5705f3bd5411844a1a97
ALhanoofe <ehanoof@outlook.com>
2026-01-21 19:12:37 +0300

    install the bcrypt and jsonwebtoken

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,5 @@
 const { User } = require('../models')
-const middleware = require('../middleware')
+const middleware = require('../models/middleware')
 
 const Register = async (req, res) => {
   try {
